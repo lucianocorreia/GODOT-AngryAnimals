88d8f052745413b76afbf35792ffe3f33da1b0a3
Luciano Correia <luciano.correia@gmail.com>
2024-12-23 15:27:42 +0400

    update

--- .godot/mono/temp/obj/Debug/AngryAnimals.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/AngryAnimals.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AngryAnimals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4618d2ad0fc8dac2f6dcf4cb9eeb918b59d67a9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AngryAnimals")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AngryAnimals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
